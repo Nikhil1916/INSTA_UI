43c2dda407cd4df990abf502707c14af243832f6
nikhil <nikhilchawla9013@gmail.com>
2024-01-15 00:27:37 +0530

    created basic Insta ui

---
+++ data.js
@@ -0,0 +1,17 @@
+const data = [
+    { id: 1, posted_image_url: 'https://source.unsplash.com/500x300/?manali', user_full_name: "Nikhil", user_name:"@himanshujain", user_image: 'https://source.unsplash.com/50x50/?man', user_story: 'https://source.unsplash.com/80x80/?man' },
+    { id: 2, posted_image_url: 'https://source.unsplash.com/500x300/?toront', user_full_name: "Himanshi", user_name:"@himanshujain", user_image: 'https://source.unsplash.com/50x50/?man', user_story: 'https://source.unsplash.com/80x80/?man' },
+    { id: 3, posted_image_url: 'https://source.unsplash.com/500x300/?thailand', user_full_name: "Nikhil", user_name:"@himanshujain", user_image: 'https://source.unsplash.com/50x50/?man', user_story: 'https://source.unsplash.com/80x80/?man' },
+    { id: 4, posted_image_url: 'https://source.unsplash.com/500x300/?mussorie', user_full_name: "Himanshi", user_name:"@himanshujain", user_image: 'https://source.unsplash.com/50x50/?man', user_story: 'https://source.unsplash.com/80x80/?man' },
+    { id: 5, posted_image_url: 'https://source.unsplash.com/500x300/?noida', user_full_name: "Nikhil", user_name:"@himanshujain", user_image: 'https://source.unsplash.com/50x50/?man', user_story: 'https://source.unsplash.com/80x80/?man' },
+    { id: 6, posted_image_url: 'https://source.unsplash.com/500x300/?delhi', user_full_name: "Himanshi", user_name:"@himanshujain", user_image: 'https://source.unsplash.com/50x50/?man', user_story: 'https://source.unsplash.com/80x80/?man' },
+    { id: 7, posted_image_url: 'https://source.unsplash.com/500x300/?punjab', user_full_name: "Nikhil", user_name:"@himanshujain", user_image: 'https://source.unsplash.com/50x50/?man', user_story: 'https://source.unsplash.com/80x80/?man' },
+    { id: 8, posted_image_url: 'https://source.unsplash.com/500x300/?US', user_full_name: "Himanshi", user_name:"@himanshujain", user_image: 'https://source.unsplash.com/50x50/?man', user_story: 'https://source.unsplash.com/80x80/?man' },
+    { id: 9, posted_image_url: 'https://source.unsplash.com/500x300/?Canada', user_full_name: "Nikhil", user_name:"@himanshujain", user_image: 'https://source.unsplash.com/50x50/?man', user_story: 'https://source.unsplash.com/80x80/?man' },
+    { id: 10, posted_image_url: 'https://source.unsplash.com/500x300/?australia', user_full_name: "Himanshi", user_name:"@himanshujain", user_image: 'https://source.unsplash.com/50x50/?man', user_story: 'https://source.unsplash.com/80x80/?man' },
+    { id: 11, posted_image_url: 'https://source.unsplash.com/500x300/?singapore', user_full_name: "Nikhil", user_name:"@himanshujain", user_image: 'https://source.unsplash.com/50x50/?man', user_story: 'https://source.unsplash.com/80x80/?man' },
+    { id: 12, posted_image_url: 'https://source.unsplash.com/500x300/?kerala', user_full_name: "Himanshi", user_name:"@himanshujain", user_image: 'https://source.unsplash.com/50x50/?man', user_story: 'https://source.unsplash.com/80x80/?man' },
+    { id: 13, posted_image_url: 'https://source.unsplash.com/500x300/?shimla', user_full_name: "Nikhil", user_name:"@himanshujain", user_image: 'https://source.unsplash.com/50x50/?man', user_story: 'https://source.unsplash.com/80x80/?man' },
+  ]
+  
+  export { data };
